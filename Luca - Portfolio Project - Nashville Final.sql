74b0f19a02f52d6b50c06e77b0b2dc58560e4685
Luca Rizzo <93331087+LucaRizzo89@users.noreply.github.com>
2021-11-07 12:03:19 +0000

    Update Luca - Portfolio Project - Nashville Final.sql

--- Luca - Portfolio Project - Nashville Final.sql
+++ Luca - Portfolio Project - Nashville Final.sql
@@ -1,5 +1,6 @@
 /* This project will focus on data cleaning of the dataset 'NashvilleHousing' */
-/* To follow along, please click on PortfolioProjects and download the 'Nashville Housing.xlsx'. */
+/* To follow along, please click on PortfolioProjects and download 
+'Nashville Housing.xlsx'. */
 
 
 SELECT *
@@ -8,8 +9,9 @@ FROM NashvilleHousing
 SELECT SaleDate
 FROM NashvilleHousing
 
-/* The first thing we will do is to change the 'SaleDate' column has it contains a timestamp '00.00.00.000' in every row, so we are going to convert
-it into a standard date format yyyy-mm-dd. */
+/* The first thing we will do is to change the 'SaleDate' column has it contains
+a timestamp '00.00.00.000' in every row, so we are going to convert it into a
+standard date format yyyy-mm-dd. */
 
 SELECT SaleDate, CONVERT(Date, SaleDate)
 FROM NashvilleHousing
@@ -25,22 +27,26 @@ ALTER TABLE NashvilleHousing DROP COLUMN SaleDate
 SELECT *
 FROM NashvilleHousing
 
-/* As we can see, the 'SaleDate' column is not anymore in our data set, while we now have the 'SaleDateConverted' column in the format that we 
-wanted. */ 
+/* As we can see, the 'SaleDate' column is not anymore in our data set, while
+we now have the 'SaleDateConverted' column in the format that we wanted. */ 
 
 SELECT PropertyAddress
 FROM NashvilleHousing
 WHERE PropertyAddress IS NULL
 
-/* Here we notice that the 'PropertyAddress' column has null values. Since from the dataset we can see that there is a relationship between
-the columns 'PropertyAddress' and 'ParcelID', then we could check whether identycal ParcelIDs are translated into identycal Property Addresses. */
+/* Here we notice that the 'PropertyAddress' column has null values. Since
+from the dataset we can see that there is a relationship between the columns
+'PropertyAddress' and 'ParcelID', then we could check whether identycal
+ParcelIDs are translated into identycal Property Addresses. */
 
 SELECT *
 FROM NashvilleHousing
 ORDER BY ParcelID
 
-/* We can see that this is the case, so we can just fill the NULL values of the 'PropertyAddress' column if the ParcelID of the same row is identycal
-to the ParcelID of another row. To do this, we will use a self join since we have the 'UniqueID' column which could be considered as a primary
+/* We can see that this is the case, so we can just fill the NULL values of
+the 'PropertyAddress' column if the ParcelID of the same row is identycal
+to the ParcelID of another row. To do this, we will use a self join since
+we have the 'UniqueID' column which could be considered as a primary
 key in this case.*/
 
 SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress
@@ -51,7 +57,8 @@ JOIN NashvilleHousing AS b
 WHERE a.PropertyAddress IS NULL
 
 
-SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress,
+ISNULL(a.PropertyAddress, b.PropertyAddress)
 FROM NashvilleHousing AS a
 JOIN NashvilleHousing AS b
 	ON a.ParcelID = b.ParcelID
@@ -72,13 +79,14 @@ SELECT *
 FROM NashvilleHousing
 WHERE PropertyAddress IS NULL
 
-/* As we can see, now we have no null values in the 'PropertyAddress' column. */
+/* As we can see, now we have no null values in the 'PropertyAddress' column.*/
 
 SELECT *
 FROM NashvilleHousing
 
-/* Now we want to split the 'PropertyAddress' column into 2 columns, a 'PropertySplitCity' column and a 'PropertySplitAddress' column. For this
-purpose we will use the SUBSTRING function.*/
+/* Now we want to split the 'PropertyAddress' column into 2 columns,
+a 'PropertySplitCity' column and a 'PropertySplitAddress' column. 
+For this purpose we will use the SUBSTRING function.*/
 
 SELECT
 SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1),
@@ -97,9 +105,11 @@ SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddres
  SELECT *
  FROM NashvilleHousing
 
-/* Now we want to do the same for the 'OwnerAddress' column, but this time we will split it into 3 columns, one for the state, one for the city and
-one for the address. This time we will use PARSENAME, in combination with the REPLACE function to convert the commas into dots, so that the PARSNAME
-function will work.*/
+/* Now we want to do the same for the 'OwnerAddress' column,
+but this time we will split it into 3 columns, one for the state,
+one for the city and one for the address. This time we will use PARSENAME,
+in combination with the REPLACE function to convert the commas into dots,
+so that the PARSNAME function will work.*/
 
 SELECT 
 PARSENAME (REPLACE (OwnerAddress, ',', '.'), 1),
@@ -119,8 +129,11 @@ OwnerSplitState = PARSENAME (REPLACE (OwnerAddress, ',', '.'), 1)
 SELECT *
  FROM NashvilleHousing
 
- /* As we can see, we managed to split both the 'PropertyAddress' and the 'OwnerAddress' columns into separate columns for the address, the city
- and the state, so that eventually in our analysis we could filter by city or state. At this point we can get rid of the original columns. */
+ /* As we can see, we managed to split both the 'PropertyAddress' 
+ and the 'OwnerAddress' columns into separate columns for the address,
+ the city and the state, so that eventually in our analysis
+ we could filter by city or state. At this point,
+ we can get rid of the original columns. */
 
  ALTER TABLE NashvilleHousing
 DROP COLUMN OwnerAddress, PropertyAddress
@@ -152,7 +165,9 @@ SET SoldAsVacant = CASE
 SELECT DISTINCT SoldAsVacant
 FROM NashvilleHousing
 
-/* Now we only have the two unique values that we wanted. Our last data cleaning step will be eliminating duplicates. This time we will use a CTE.*/
+/* Now we only have the two unique values that we wanted.
+Our last data cleaning step will be eliminating duplicates.
+This time we will use a CTE.*/
 
 SELECT *, ROW_NUMBER() OVER(
 		  PARTITION BY ParcelID,
@@ -222,7 +237,8 @@ SELECT *
 FROM CTE_row_num
 WHERE row_num > 1 
 
-/* The output shows no rows this time, which means that our dataset doesn't contain any duplicates now. */
+/* The output shows no rows this time,
+which means that our dataset doesn't contain any duplicates now. */
 
 
 
